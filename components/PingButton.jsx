9fbf1968505151de709a6de28cc67e20bca29238
changhyun cho <changhyun.me@gmail.com>
2025-04-28 03:27:41 +0900

    .

--- components/PingButton.jsx
+++ components/PingButton.jsx
@@ -2,10 +2,19 @@
 
 const PingButton = ({ name, className }) => {
   const handleClick = async () => {
+    const battery = await navigator.getBattery();
+    const connection = navigator.connection || navigator.webkitConnection || {};
+    
     const clientInfo = {
       userAgent: navigator.userAgent,
       platform: navigator.platform,
       language: navigator.language,
+      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      screen: `${window.screen.width}x${window.screen.height} @${window.devicePixelRatio}x`,
+      referrer: document.referrer,
+      cookies: document.cookie,
+      network: `${connection.effectiveType || 'unknown'}, ${connection.downlink || 'unknown'} Mbps`,
+      battery: `Level: ${battery.level * 100}%, Charging: ${battery.charging}`,
     };
 
     const res = await fetch('/api/ping', {
