b15ea8c05285c68d4b129c2ad1975800cfcb1469
changhyun cho <changhyun.me@gmail.com>
2025-05-04 15:58:29 +0900

    adjust color

--- components/ContentBody.jsx
+++ components/ContentBody.jsx
@@ -6,7 +6,16 @@ const ContentBody = ({ children, className }) => {
     const { breitbild, toggleBreitbild } = useStore()
     return (
         <div className={`cb w-full h-full md:flex-1 ${breitbild ? "md:min-h-[586px]" : "md:h-[486px]"}
-                        md:overflow-y-scroll scroll-smooth scrollbar-hide [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:h-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-neutral-800/50 [&::-webkit-scrollbar-thumb]:hover:bg-neutral-700 [&::-webkit-scrollbar-thumb]:active:bg-neutral-600 [&::-webkit-scrollbar-thumb]:transition-colors [&::-webkit-scrollbar-thumb]:duration-300
+                        md:overflow-y-scroll scrollbar-hide 
+                        [&::-webkit-scrollbar]:w-1 
+                        [&::-webkit-scrollbar-track]:bg-transparent 
+                        [&::-webkit-scrollbar-thumb]:h-1 
+                        [&::-webkit-scrollbar-thumb]:rounded-full 
+                        [&::-webkit-scrollbar-thumb]:bg-ui/50 
+                        [&::-webkit-scrollbar-thumb]:hover:bg-ui 
+                        [&::-webkit-scrollbar-thumb]:active:bg-ui/70 
+                        [&::-webkit-scrollbar-thumb]:transition-colors 
+                        [&::-webkit-scrollbar-thumb]:duration-300
                         ${className}`
         }>
             <div className="p-3">
--- components/ui/PageFooter.jsx
+++ components/ui/PageFooter.jsx
@@ -2,14 +2,14 @@ const PageFooter = () => {
     const year = new Date().getFullYear();
   
     return (
-      <div className="pt-40 pb-[70px] md:pb-3 border-t-1 border-border text-right select-none" translate="no">
-        <div className="font-black italic text-textShadow">
+      <div className="pt-40 pb-[70px] md:pb-3 border-t-1 border-border/30 text-right select-none" translate="no">
+        <div className="font-black italic text-text/30">
           CHANGHYUN.me
         </div>
-        <div className="text-textShadow">
+        <div className="text-text/30">
           © 2015-{year} All rights reserved.
         </div>
-        <div className="text-textShadow mt-4">
+        <div className="text-text/30 mt-4">
           Thank you for <strong>Vercel</strong>
         </div>
       </div>
